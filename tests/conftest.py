eded1dfa490a5c9b521dfaa1763820254a55c8d3
Anastasia Titova <atitova1294@gmail.com>
2025-05-19 23:17:42 +0300

    init commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,32 @@
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture(scope="function")
 def browser_options():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "127.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@https://selenoid.autotests.cloud//wd/hub",
+        options=options)
+
+    browser.config.driver = driver
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url='https://demoqa.com'
 
-    yield
+    yield browser
 
     browser.quit()
 
